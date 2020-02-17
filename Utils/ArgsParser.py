3fc4cfad299a9b4eded40cfc5301671db15f00b1
Ben071 <benjaminhiggs@icloud.com>
2020-02-17 17:04:29 +0000

    Add args parser

---
+++ Utils/ArgsParser.py
@@ -0,0 +1,16 @@
+class ArgsParser:
+    def __init__(self, client):
+        self.client = client
+
+    def get_args(self, message, *args):
+        args = self.client.API.request(
+            route="argparser",
+            Message=" ".join(args)
+        ).get("Message", [])
+
+        if args == -1:
+            return self.client.Errors.NoAPIConnection().send(
+                message.channel
+            )
+
+        return args
