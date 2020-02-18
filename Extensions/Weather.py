04330d37dfea9f23e79159a81be68e8ba7ff7dc0
Ben071 <benjaminhiggs@icloud.com>
2020-02-18 00:47:04 +0000

    Add forecast command

--- Extensions/Weather.py
+++ Extensions/Weather.py
@@ -1,6 +1,7 @@
 from discord import Embed, Colour
 from geopy.geocoders import Nominatim
 import re
+import datetime
 
 
 def get_output(w_text):
@@ -222,7 +223,8 @@ class Weather:
             value="Low of {}°C ({}°F)".format(minc, minf),
             inline=True
         ).set_footer(
-            text="Lat: {} | Lon: {}".format(lat, lon) if (place != lat or country != lon) else ""
+            text="Lat: {} | Lon: {} | Powered by OpenWeatherMap".format(lat, lon) if (
+                        place != lat or country != lon) else "Powered by OpenWeatherMap"
         )
 
         return embed
@@ -282,8 +284,146 @@ class Weather:
         )
 
 
+class Forecast:
+    @staticmethod
+    def get_weather_text(r={}):
+        # Returns a string representing the weather passed
+        main = r["main"]
+        weath = r["weather"]
+
+        # Make sure we get the temps in both F and C
+        tc = k_to_c(main["temp"])
+        tf = c_to_f(tc)
+        minc = k_to_c(main["temp_min"])
+        minf = c_to_f(minc)
+        maxc = k_to_c(main["temp_max"])
+        maxf = c_to_f(maxc)
+
+        # Gather the formatted conditions
+        condition_list = []
+        for x, y in enumerate(weath):
+            d = y["description"]
+            if x == 0:
+                d = d.capitalize()
+            condition_list.append(get_output(d))
+        condition = ", ".join(condition_list)
+
+        # Format the description
+        desc = "{}°F ({}°C),\n\n{},\n\nHigh of {}°F ({}°C) - Low of {}°F ({}°C)\n\n".format(
+            tf, tc,
+            condition,
+            maxf, maxc,
+            minf, minc
+        )
+
+        return desc
+
+    def __init__(self, client):
+        self.client = client
+
+        self.name = "forecast"
+        self.aliases = [
+            "weatherforecast"
+        ]
+
+        self.category = "Utilities"
+        self.perm_level = 0
+        self.description = "Gets some weather, for 5 days."
+        self.usage = "forecast <place_name>"
+        self.geo = Nominatim(user_agent="TFR-Bot")
+        self.key = self.client.config.openweathermap_key
+
+    async def run(self, _, message, *args):
+        if len(args) == 0:
+            return await self.client.Errors.MissingArgs(
+                "place_name"
+            ).send(
+                message.channel
+            )
+
+        self.client.ArgsParser.get_args(
+            message,
+            *args
+        )
+
+        place_name = " ".join(args)
+
+        # Strip anything that's non alphanumeric or a space
+        place_name = re.sub(r'([^\s\w]|_)+', '', str(place_name))
+        location = self.geo.geocode(str(place_name))
+
+        if location is None:
+            return await self.client.Errors.PlaceNotFound(
+                place_name
+            ).send(
+                message.channel
+            )
+
+        # We want the 5-day forecast at this point
+        r = await self.client.DL.async_json(
+            "http://api.openweathermap.org/data/2.5/forecast?appid={}&lat={}&lon={}".format(
+                self.key,
+                location.latitude,
+                location.longitude
+            ))
+
+        try:
+            city = r["city"]
+            city_name = city["name"]
+            country = city["country"]
+            flag = ":flag_{}:".format(str(country).lower())
+
+            title = "5 Day Forecast for {}, {} {}".format(city_name, country, flag)
+        except KeyError:
+            title = "5 Day Forecast for {}".format(location)
+
+        days = {}
+        for x in r["list"]:
+            # Check if the day exists - if not, we set up a pre-day
+            day = x["dt_txt"].split(" ")[0]
+            is_noon = "12:00:00" in x["dt_txt"]
+            if day not in days:
+                days[day] = {
+                    "main": x["main"],
+                    "weather": x["weather"],
+                    "day_count": 1
+                }
+                continue
+            # Day is in the list - let's check values
+            if x["main"]["temp_min"] < days[day]["main"]["temp_min"]:
+                days[day]["main"]["temp_min"] = x["main"]["temp_min"]
+            if x["main"]["temp_max"] > days[day]["main"]["temp_max"]:
+                days[day]["main"]["temp_max"] = x["main"]["temp_max"]
+            # Add the temp
+            days[day]["main"]["temp"] += x["main"]["temp"]
+            days[day]["day_count"] += 1
+            # Set the weather data if is noon
+            if is_noon:
+                days[day]["weather"] = x["weather"]
+
+        embed = Embed(
+            type="rich",
+            title=title,
+            color=Colour.from_rgb(111, 255, 141)
+        ).set_footer(
+            text="Powered by OpenWeatherMap"
+        )
+
+        for day in sorted(days):
+            # Average the temp, strip weather duplicates
+            days[day]["main"]["temp"] /= days[day]["day_count"]
+            embed.add_field(
+                name=datetime.datetime.strptime(day, "%Y-%m-%d").strftime("%A, %b %d, %Y") + ":",
+                value=self.get_weather_text(days[day]),
+                inline=False
+            )
+
+        await message.channel.send(embed=embed)
+
+
 def setup(client):
     client.CommandHandler.add_commands(
         TempConvert(client),
-        Weather(client)
+        Weather(client),
+        Forecast(client)
     )
