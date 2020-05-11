b8e846554c4af8a262e3674ee123ecb2aff143fb
Jack <40802798+Jack073@users.noreply.github.com>
2020-05-11 16:21:21 +0100

    Improve code quality, add webhook error logging, add image filtering

---
+++ Events/on_error.py
@@ -0,0 +1,16 @@
+from traceback import format_exc
+
+
+class ErrorHandler:
+	def __init__(self, client):
+		self.client = client
+		self.client.event(self.on_error)
+		
+	async def on_error(self, event, *_, **__):
+		await self.client.WebhookManager.send(
+			f"Exception in `{event}`\n```py\n{format_exc()}```"
+		)
+
+		
+def setup(client):
+	ErrorHandler(client)
